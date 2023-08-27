27eb237534723eda96ae56c10a0521dde1b1a453
Anushka Pandey <anushkaa.pandey1@gmail.com>
2023-08-27 15:55:08 +0530

    frontend created

--- server.js
+++ server.js
@@ -8,6 +8,22 @@ const operationHistory = loadHistory();
 
 app.use(express.urlencoded({ extended: true }));
 
+const performOperation = (exp) => {
+    switch (operator) {
+        case 'plus':
+            return num1 + num2;
+        case 'minus':
+            return num1 - num2;
+        case 'into':
+            return num1 * num2;
+        case 'divide':
+            return num1 / num2;
+        case 'power':
+            return math.pow(num1, num2);
+        default:
+            throw new Error('Invalid operator.');
+    }
+};
 function saveHistory() {
     fs.writeFileSync('operation-history.json', JSON.stringify(operationHistory));
 }
@@ -20,10 +36,30 @@ function loadHistory() {
         return [];
     }
 }
+app.use(express.static(__dirname + '/public'));
+app.get('/history-json', (req, res) => {
+    // var data = require('./operation-history.json')
+    //console.log(data)
+    // res.send(operationHistory)
+    const history = loadHistory();
+    res.json(history);
+    //res.sendFile(__dirname + '/public/output.html');
 
-app.get('/history', (req, res) => {
-    res.json(operationHistory);
 });
+//res.sendFile(__dirname + '/public/output.html');
+app.use(express.static(__dirname + '/public'));
+// Serve the HTML file
+app.get('/history', (req, res) => {
+    fs.readFile(__dirname + '/public/output.html', 'utf8', (err, data) => {
+        if (err) {
+            res.status(500).send('Error reading HTML file.');
+        } else {
+            res.send(data);
+        }
+    });
+});
+
+
 app.get('/:first*', (req, res) => {
     console.log(req.params)
     first = req.params.first
@@ -35,6 +71,7 @@ app.get('/:first*', (req, res) => {
         .replace(/minus/g, '-')
         .replace(/into/g, '*')
         .replace(/over/g, '/')
+        .replace(/power/g, '^')
     try {
         const result = math.evaluate(exp);
         const response = { exp, result };
@@ -44,17 +81,19 @@ app.get('/:first*', (req, res) => {
         if (operationHistory.length > 20) {
             operationHistory.shift();
         }
-
+        saveHistory();
         res.json(response);
     } catch (error) {
         res.status(400).send(error.message);
     }
-    saveHistory();
-    res.json(response);
+    // saveHistory();
+    //res.json(response);
 });
+// Route to get the history of operations
+
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 const PORT = 3000;
